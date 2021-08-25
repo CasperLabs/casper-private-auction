e0422a8145b7800a030d3fd7e9e9184af0d9aaec
alexlimonov <alimonov@casperlabs.io>
2021-08-25 15:40:49 -0500

    Fixes to ensure NFT deployment works with reorg_lib

--- nctl-tests/setup/actions/deploy_nft.sh
+++ nctl-tests/setup/actions/deploy_nft.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 NFT_INSTALL_DEPLOY=$(casper-client put-deploy\
-	--chain-name $NETWORK_NAME\
-	--node-address $NODE_1_ADDRESS\
-	--secret-key $USER_1_SECRET_KEY\
-	--payment-amount $GAS_LIMIT\
-	--session-path $NFT_WASM\
-	--session-arg "token_name:string='Dragon'"\
-	--session-arg "token_symbol:string='DRG'"\
-	--session-arg "token_meta:string=''"\
-	| jq .result.deploy_hash\
-	| tr -d '"')
+  --chain-name $NETWORK_NAME\
+  --node-address $NODE_1_ADDRESS\
+  --secret-key $USER_1_SECRET_KEY\
+  --payment-amount $GAS_LIMIT\
+  --session-path $NFT_WASM\
+  --session-arg "name:string='Dragon'"\
+  --session-arg "symbol:string='DRG'"\
+  --session-arg "meta:string=''"\
+  --session-arg "contract_name:string='AuctionTestToken'"\
+  | jq .result.deploy_hash\
+  | tr -d '"')
--- nctl-tests/setup/misc/client_put_deploy_config.sh
+++ nctl-tests/setup/misc/client_put_deploy_config.sh
@@ -10,4 +10,4 @@ USER_4_SECRET_KEY=$NCTL/assets/net-1/users/user-4/secret_key.pem
 USER_5_SECRET_KEY=$NCTL/assets/net-1/users/user-5/secret_key.pem
 GAS_LIMIT=1000000000000
 AUCTION_WASM=~/CasperLabs/casper-private-auction/target/wasm32-unknown-unknown/release/casper-private-auction-installer.wasm
-NFT_WASM=~/CasperLabs/casper-nft-cep47/target/wasm32-unknown-unknown/release/dragons-nft.wasm
+NFT_WASM=~/CasperLabs/casper-nft-cep47/target/wasm32-unknown-unknown/release/cep47-token.wasm
