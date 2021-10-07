db37299a22445e1377597c0db4cbee3aca1939fd
alexlimonov <alimonov@casperlabs.io>
2021-10-07 12:40:34 -0500

    Beginning of a rework to use complex args

--- nctl-tests/setup/misc/client_put_deploy_config.sh
+++ nctl-tests/setup/misc/client_put_deploy_config.sh
@@ -9,9 +9,11 @@ USER_3_SECRET_KEY=$NCTL/assets/net-1/users/user-3/secret_key.pem
 USER_4_SECRET_KEY=$NCTL/assets/net-1/users/user-4/secret_key.pem
 USER_5_SECRET_KEY=$NCTL/assets/net-1/users/user-5/secret_key.pem
 GAS_LIMIT=1000000000000
-AUCTION_WASM=~/CasperLabs/casper-private-auction/target/wasm32-unknown-unknown/release/casper-private-auction-installer.wasm
-#NFT_WASM=~/CasperLabs/casper-nft-cep47/target/wasm32-unknown-unknown/release/cep47-token.wasm
-NFT_WASM=~/CasperLabs/CaskNFT/target/wasm32-unknown-unknown/release/cask-token.wasm
+#AUCTION_WASM=~/CasperLabs/casper-private-auction/target/wasm32-unknown-unknown/release/casper-private-auction-installer.wasm
+#NFT_WASM=~/CasperLabs/CaskNFT/target/wasm32-unknown-unknown/release/cask-token.wasm
+AUCTION_WASM=../tests/wasm/casper-private-auction-installer.wasm
+KYC_WASM=setup/fixtures/contracts/civic-token.wasm
+NFT_WASM=setup/fixtures/contracts/cask-token.wasm
 
 # Make sure rust-script output is readable
 export RUST_LOG=rust-script=error
