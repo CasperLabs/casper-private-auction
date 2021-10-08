f83c446dc0d219bb0044f273b775df94dfbc8ce0
alexlimonov <alimonov@casperlabs.io>
2021-10-07 20:30:32 -0500

    Additional work on integrating KYC and commissions

--- nctl-tests/setup/misc/client_put_deploy_config.sh
+++ nctl-tests/setup/misc/client_put_deploy_config.sh
@@ -11,7 +11,7 @@ USER_5_SECRET_KEY=$NCTL/assets/net-1/users/user-5/secret_key.pem
 GAS_LIMIT=1000000000000
 #AUCTION_WASM=~/CasperLabs/casper-private-auction/target/wasm32-unknown-unknown/release/casper-private-auction-installer.wasm
 #NFT_WASM=~/CasperLabs/CaskNFT/target/wasm32-unknown-unknown/release/cask-token.wasm
-AUCTION_WASM=../tests/wasm/casper-private-auction-installer.wasm
+AUCTION_WASM=../../../tests/wasm/casper-private-auction-installer.wasm
 KYC_WASM=setup/fixtures/contracts/civic-token.wasm
 NFT_WASM=setup/fixtures/contracts/cask-token.wasm
 
