1cfd9e004ef77caab2e7d057b71243546f8de9ac
alexlimonov <alimonov@casperlabs.io>
2021-10-13 15:17:36 -0500

    Minimal working tests with KYC tokens

--- nctl-tests/setup/actions/mint_kyc.sh
+++ nctl-tests/setup/actions/mint_kyc.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
+CWD_KYC=$(pwd)
+parent_path_KYC=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "$parent_path_KYC"
+
 KYC_RECIPIENT_ARG=$1
+
+# Note that this recreates all of the .json files every time
+cd ../fixtures
+./metacask-runtime-arg-builder $KYC_RECIPIENT_ARG $KYC_RECIPIENT_ARG "artist,$BUYER_5_KEY,100|broker,$BUYER_4_KEY,200"
+cd "$parent_path_KYC"
 
 KYC_MINT_DEPLOY=$(casper-client put-deploy\
         --chain-name $NETWORK_NAME\
@@ -9,8 +18,8 @@ KYC_MINT_DEPLOY=$(casper-client put-deploy\
         --payment-amount $GAS_LIMIT\
         --session-hash $KYC_CONTRACT_HASH\
         --session-entry-point "mint"\
-        --session-arg "recipient:key='$KYC_RECIPIENT_ARG'"\
-        --session-arg "token_id:opt_string=null"\
-        --session-arg "token_meta:string=''"\
+        --session-args-complex "../fixtures/arg-files/kyc_mint_args.json"\
         | jq .result.deploy_hash\
         | tr -d '"')
+
+cd $CWD_KYC
